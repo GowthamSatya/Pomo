e491fa90149fc95d84a5b1484c137ba580b1fddf
GowthamSatya <gowthamsatya5@gmail.com>
2022-07-12 01:55:59 +0530

    added Themed Buttons --noAnimations

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,42 @@ import { useState } from 'react'
 import { PomoState, Timer, ButtonGroup } from "./components"
 import './App.css'
 import { useTheme } from "./utils/ThemeWrapper"
+import { MdNightlightRound } from "react-icons/md"
+import { FiSun } from "react-icons/fi"
 
 
 function App() {
   const { darkTheme, toggleTheme, updateColor, color } = useTheme();
   console.log(darkTheme, color);
   return (
-    <div className={`h-screen ${color.backgroundColor} flex items-center justify-center`}>
-      <div className='flex flex-col'>
-        <button onClick={toggleTheme}>
-          ToggleTheme
+    <div className={`h-screen overflow-hidden ${color.backgroundColor}`}>
+      <div className='w-full flex items-center justify-end p-4'>
+        <button className='m-3' onClick={toggleTheme}>
+
+          {darkTheme ? <FiSun size={30} className='text-mygray-50' /> : <MdNightlightRound className='text-black' size={30} />}
         </button>
 
-        <button onClick={() => updateColor('green')}>
-          Green
+        <button className={`w-8 h-8 m-2 bg-mygreen-400 rounded-full border-2 ${darkTheme ? "border-mygray-50" : "border-black"}`} onClick={() => updateColor('green')}>
+
         </button>
-        <button onClick={() => updateColor('blue')}>
-          Blue
-        </button>
-        <button onClick={() => updateColor('red')}>
-          Red
-        </button>
+        <button className={`w-8 h-8 m-2 bg-myblue-400 rounded-full border-2 ${darkTheme ? "border-mygray-50" : "border-black"}`} onClick={() => updateColor('blue')}></button>
+        <button className={`w-8 h-8 m-2 bg-myred-400 rounded-full border-2 ${darkTheme ? "border-mygray-50" : "border-black"}`} onClick={() => updateColor('red')}></button>
+      </div>
+
+      <div className={`flex h-full items-center justify-center`}>
+
+        <div className='flex flex-col'>
 
 
-        <PomoState />
-        <Timer />
-        <ButtonGroup />
+
+
+          <PomoState />
+          <Timer />
+          <ButtonGroup />
+        </div>
       </div>
     </div>
+
   )
 }
 
